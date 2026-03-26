2630d5323de68c14d36b0b2d483473d343be9242
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 20:09:39 -0300

    Exercicio 08 finalizado

---
+++ src/Exercicio8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+
+        Scanner ent = new Scanner(System.in);
+
+
+        int valor, valorDezena;
+
+        //
+
+        System.out.println("Digite o Valor de 3 Digitos (100 - 999) --> ");
+        valor = ent.nextInt();
+
+
+        valorDezena = valor % 100 /10;
+
+        System.out.println("O Valor da Dezena deste número é --> " + valorDezena);
+        valorDezena = ent.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
